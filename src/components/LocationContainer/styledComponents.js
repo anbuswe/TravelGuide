e582bb8d189cc5ca622ab3ef75ef7a6e8461a5a4
anbu <arasuanbu586@gmail.com>
2023-11-24 10:30:32 +0000

    first commit

---
+++ src/components/LocationContainer/styledComponents.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components/macro'
+
+export const LocationCardContainer = styled.div`
+  width: 80%;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  margin: 20px;
+`
+
+export const HeadingDescriptionContainer = styled.div`
+  padding: 10px;
+`
+
+export const Heading = styled.h1`
+  font-size: 15px;
+`
+
+export const Paragraph = styled.p`
+  font-size: 15px;
+`
